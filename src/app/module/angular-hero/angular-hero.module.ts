d9662c4f64f253304512e52c7a1023e3e872ddde
Thilini-Weerasinghe <thiliniweerasinghe09@gmail.com>
2018-11-10 12:29:43 +0530

    create components

--- src/app/module/angular-hero/angular-hero.module.ts
+++ src/app/module/angular-hero/angular-hero.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WorldComponent } from './world/world.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [WorldComponent, HomeComponent]
 })
 export class AngularHeroModule { }
